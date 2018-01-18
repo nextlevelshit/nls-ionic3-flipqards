9f7956a55390108824c72208c4b6ac35ea6ef60b
Michael Czechowski <mail@dailysh.it>
2018-01-18 18:07:13 +0100

    safe running

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,11 @@ import { Component, ViewChild } from '@angular/core';
 import { Nav, Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { createConnection } from 'typeorm'
 
 import { HomePage } from '../pages/home/home';
 import { ListPage } from '../pages/list/list';
+import { Category } from '../entities/category';
 
 @Component({
   templateUrl: 'app.html'
@@ -16,7 +18,11 @@ export class MyApp {
 
   pages: Array<{title: string, component: any}>;
 
-  constructor(public platform: Platform, public statusBar: StatusBar, public splashScreen: SplashScreen) {
+  constructor(
+    public platform: Platform,
+    public statusBar: StatusBar,
+    public splashScreen: SplashScreen
+  ) {
     this.initializeApp();
 
     // used for an example of ngFor and navigation
@@ -28,11 +34,54 @@ export class MyApp {
   }
 
   initializeApp() {
-    this.platform.ready().then(() => {
+    this.platform.ready().then(async () => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       this.statusBar.styleDefault();
       this.splashScreen.hide();
+
+      let connection;
+
+      // if (window.cordova) {
+        // running on device/emulator
+        connection = await createConnection({
+          type: 'cordova',
+          database: 'test',
+          location: 'default',
+          logging: ['error', 'query', 'schema'],
+          synchronize: true,
+          entities: [
+            Category
+          ]
+        });
+      // } else {
+      //   // running in dev mode
+      //   connection = await createConnection({
+      //     type: 'websql',
+      //     database: 'testing',
+      //     version: '1',
+      //     description: 'test database',
+      //     size: 2 * 1024 * 1024,
+      //     entities: [
+      //       Category
+      //     ],
+      //     logging: true,
+      //     synchronize: true
+      //   });
+      // }
+
+      // await createConnection({
+      //   type: 'cordova',
+      //   database: 'test',
+      //   location: 'default',
+      //   logging: ['error', 'query', 'schema'],
+      //   synchronize: true,
+      //   entities: [
+      //     // Author,
+      //     // Category,
+      //     // Post
+      //   ]
+      // });
     });
   }
 
--- src/pages/list/list.ts
+++ src/pages/list/list.ts
@@ -1,37 +1,45 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
+import { getRepository, Repository } from 'typeorm';
+
+import { Category } from './../../entities/category';
 
 @Component({
   selector: 'page-list',
   templateUrl: 'list.html'
 })
 export class ListPage {
+  private loadedCategories: boolean = false;
+  private loadedCategory: Category = null;
+
   selectedItem: any;
-  icons: string[];
-  items: Array<{title: string, note: string, icon: string}>;
+  categories: Category[];
+  sampleCategories: Array<string> = ['Geschichte im Überblick', 'Philosophie'];
+  sampleCategory: string = 'Geschichte im Überblick';
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
-    // If we navigated to this page, we will have an item available as a nav param
     this.selectedItem = navParams.get('item');
-
-    // Let's populate this page with some filler content for funzies
-    this.icons = ['flask', 'wifi', 'beer', 'football', 'basketball', 'paper-plane',
-    'american-football', 'boat', 'bluetooth', 'build'];
-
-    this.items = [];
-    for (let i = 1; i < 11; i++) {
-      this.items.push({
-        title: 'Item ' + i,
-        note: 'This is item #' + i,
-        icon: this.icons[Math.floor(Math.random() * this.icons.length)]
-      });
-    }
   }
 
-  itemTapped(event, item) {
-    // That's right, we're pushing to ourselves!
-    this.navCtrl.push(ListPage, {
-      item: item
-    });
+  ionViewDidLoad() {
+    this.runDemo();
+  }
+
+  async runDemo() {
+
+    // this.sampleCategories.forEach(name => {
+      let newCategory = new Category();
+      newCategory.name = this.sampleCategory;
+      // newCategory.name = name;
+    // });
+
+    const categoryRepository = getRepository('category') as Repository<Category>;
+    await categoryRepository.save(newCategory);
+
+    console.log('Category has been saved');
+
+    const loadedCatgory = await categoryRepository.createQueryBuilder('category')
+      .where('category.id = :id', {id: newCategory.id})
+      .getOne();
   }
 }
