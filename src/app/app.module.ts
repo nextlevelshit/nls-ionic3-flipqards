96d783cf529bddb0e30fcda370eea183cf6e2c0b
Michael Czechowski <mail@dailysh.it>
2018-01-26 16:14:41 +0100

    animations running more or less

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
@@ -19,6 +20,7 @@ import { LearningPage } from './../pages/learning/learning';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     IonicModule.forRoot(MyApp),
   ],
   bootstrap: [IonicApp],
--- src/pages/learning/learning.ts
+++ src/pages/learning/learning.ts
@@ -1,19 +1,84 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { trigger, state, style, transition, animate } from '@angular/animations';
 
 import { Category } from './../../entities/category';
 import { Card } from './../../entities/card';
+import { Observable } from 'rxjs/Rx';
 
 @IonicPage()
 @Component({
   selector: 'page-learning',
   templateUrl: 'learning.html',
+  animations: [
+    trigger('card', [
+      // state('show', style({
+      //   transform: 'translate3d(0,0,0)',
+      //   filter: 'blur(0)',
+      //   opacity: 1
+      // })),
+      // state('swipeLeft', style({
+      //   transform: 'translateX(-100%)',
+      //   opacity: 0
+      // })),
+      transition(':enter', [
+        style({
+          transform: 'translate3d(0,0,-100vh)',
+          filter: 'blur(10px)',
+          opacity: 0
+        }),
+        animate(500, style({
+          transform: 'translate3d(0,0,0)',
+          filter: 'blur(0)',
+          opacity: 1
+        }))
+      ]),
+      transition(':leave', [
+        style({
+          transform: 'translate3d(0,0,-100vh)',
+          filter: 'blur(10px)',
+          opacity: 0
+        }),
+        animate(500, style({
+          transform: 'translate3d(0,0,0)',
+          filter: 'blur(0)',
+          opacity: 1
+        }))
+      ]),
+      transition('* => swipeRight', [
+        style({
+          transform: 'translate3d(0,0,0)',
+          filter: 'blur(0)',
+          opacity: 1
+        }),
+        animate(800, style({
+          transform: 'translate3d(50vw,0,0)',
+          filter: 'blur(10px)',
+          opacity: 0
+        }))
+      ]),
+      transition('* => swipeLeft', [
+        style({
+          transform: 'translate3d(0,0,0)',
+          filter: 'blur(0)',
+          opacity: 1
+        }),
+        animate(800, style({
+          transform: 'translate3d(-50vw,0,0)',
+          filter: 'blur(10px)',
+          opacity: 0
+        }))
+      ])
+    ])
+  ]
 })
 export class LearningPage {
   category: Category = this.navParams.get('category');
   cards: Card[];
   run;
-  flipped: false;
+  flipped: boolean = false;
+  cardState: string;
+  active: boolean = true;
 
   constructor(
     public navCtrl: NavController,
@@ -28,17 +93,22 @@ export class LearningPage {
       correct: 0,
       total: this.category.cards.length
     };
-
-    console.log(this.category.cards);
   }
 
   public next(answer: boolean) {
     if (answer) {
+      this.cardState = 'swipeRight';
       this.increaseCorrect(this.getCurrent());
     } else {
       this.increaseWrong(this.getCurrent());
+      this.cardState = 'swipeLeft';
     }
-    this.category.cards.shift();
+    this.flipped = false;
+
+    Observable.interval(1000).take(1).subscribe(() => {
+      this.category.cards.shift();
+      this.cardState = undefined;
+    });
   }
 
   public getCurrent() {
